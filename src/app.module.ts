acb3661d75e13ac082e8c9e60de38a45d60c8d4a
dorito <gbrl.ferreira@outlook.com>
2022-02-24 20:20:01 -0300

    Updated TypeORM connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,11 +9,7 @@ import { AppService } from './app.service';
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
+      url: process.env.DB_URL,
       entities: [__dirname + '/entities/{.ts,.js}'],
       synchronize: process.env.DB_SYNCHRONIZE === 'true',
     }),
